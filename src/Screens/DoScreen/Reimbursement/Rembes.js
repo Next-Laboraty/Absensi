1e78abe4e158e4384d924ac0a1c9ab63f8e5f5b2
AgungPri <agunggenresis@gmail.com>
2022-05-10 11:50:41 +0700

    #9 |AgungPri|UI Screen Rembes

--- src/Screens/DoScreen/Reimbursement/Rembes.js
+++ src/Screens/DoScreen/Reimbursement/Rembes.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect } from 'react'
-import { Text, View, TextInput,ScrollView } from 'react-native'
+import { Text, View, TextInput, ScrollView } from 'react-native'
 import { MaterialIcons, MaterialCommunityIcons, Ionicons, FontAwesome, AntDesign, Entypo, EvilIcons, Octicons } from '@expo/vector-icons';
 import MaintenanceScreen from '../../../Molecule/MaintenanceScreen'
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -11,10 +11,10 @@ export default function Rembes() {
     console.log(employee)
     return (
         <View style={{ flex: 1, paddingHorizontal: 10 }}>
-            <ScrollView style={{ backgroundColor: '#FFFFFE', marginTop: 5, borderRadius: 20, flex:1 }}>
+            <ScrollView style={{ backgroundColor: '#FFFFFE', marginTop: 5, borderRadius: 20, flex: 1 }}>
                 <View style={{ flexDirection: 'row', marginTop: 10, justifyContent: 'space-between' }}>
                     <Text style={{ fontFamily: 'Medium', fontSize: 11, color: '#2C3333', paddingLeft: 7 }}>{employee.employee_name}</Text>
-                    <Text style={{ fontFamily: 'Medium', fontSize: 15, color: '#2C3333', paddingLeft: 7, fontWeight: 'bold' }}>{employee.designation}</Text>
+                    <Text style={{ fontFamily: 'Medium', fontSize: 15, color: '#ADACAC', paddingLeft: 7, fontWeight: 'bold', marginRight: 15 }}>{employee.designation}</Text>
                 </View>
                 <View style={{ marginTop: 10, marginHorizontal: 10 }}>
                     <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Masukan Tanggal</Text>
@@ -25,7 +25,7 @@ export default function Rembes() {
                 <View style={{ marginTop: 10, marginHorizontal: 10 }}>
                     <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Tipe Klaim</Text>
                     <TextInput placeholder='' style={{
-                        borderWidth: 1, fontSize: 10,
+                        borderWidth: 1, fontSize: 10
                     }} />
                 </View>
                 <View style={{ marginTop: 10, marginHorizontal: 10 }}>
@@ -40,12 +40,15 @@ export default function Rembes() {
                         borderWidth: 1, fontSize: 10,
                     }} />
                 </View>
-                <View style={{ margin: 10}}>
-                    <Button status={"danger"}>Upload Bukti </Button>
-                    
+                <View style={{ marginTop: 40, marginHorizontal: 20 }}>
+                    <Button status={"danger"} style={{ borderWidth: 1, marginHorizontal: 100, borderRadius: 15, fontFamily: 'Regular' }}>Upload Bukti </Button>
+
+                </View>
+                <View style={{margin: 20, flex: 1, backgroundColor: '#E6E6E6', height: 170}}>
+                <Text style={{alignSelf: 'center', marginTop: 70, color: 'white', fontFamily: 'Regular', fontSize: 20, fontWeight: 'bold' }}>Preview</Text>
                 </View>
             </ScrollView>
-            <Button status={"info"} style={{marginVertical:10}}>Ajukan</Button>
+            <Button status={"info"} style={{ marginVertical: 10, marginHorizontal:10 }}>Ajukan</Button>
         </View>
     )
 }
