e8ef3c708b41559b1813ecb27a1634fa1ece90d1
Kevin Adhi Krisma <kepsgurih@pm.me>
2022-05-12 14:05:57 +0700

    #14 | Kevin Adhi Krisma |Penambahan Backend pada Issue

--- src/Screens/DoScreen/Asuransi/index.js
+++ src/Screens/DoScreen/Asuransi/index.js
@@ -1,73 +1,89 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, TextInput, ScrollView, Picker } from 'react-native'
+import { Text, View, TextInput, ScrollView, Picker, StyleSheet } from 'react-native'
 import { MaterialIcons, MaterialCommunityIcons, Ionicons, FontAwesome, AntDesign, Entypo, EvilIcons, Octicons } from '@expo/vector-icons';
 import MaintenanceScreen from '../../../Molecule/MaintenanceScreen'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useSelector } from 'react-redux';
 import { Button } from '@ui-kitten/components';
+import axios from 'axios';
+import AxiosPostData from '../../../lib/AxiosPostData';
+import LoadingComp from '../../../Atomic/LoadingComp';
 
 export default function Asuransi() {
-    const [selectPriority, setSelectPriority] = useState("High")
+    const [priority, setPriority] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const { server, token, employee } = useSelector(state => state.employee)
+    let priodata = {
+        server,
+        token
+    }
+    useEffect(() => {
+        AxiosPostData(`http:///103.179.57.18:21039/Issue/priority`, token, priodata).then(res => {
+            setPriority(res.data)
+            setLoading(false)
+        })
+            .catch(err => alert('Error'))
+    }, [])
+    const [selectPriority, setSelectPriority] = useState("Pilih")
+    if (priority === null)
+        return <LoadingComp />
     return (
-        <View style={{ flex: 1, paddingHorizontal: 10 }}>
-            <ScrollView style={{ backgroundColor: '#FFFFFE', marginTop: 5, borderRadius: 20, flex: 1 }}>
-                <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                    <Text style={{ fontFamily: 'Medium', fontSize: 11, color: '#2C3333', paddingLeft: 7 }}>AgungPriyatna</Text>
-                </View>
-                <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                    <Text style={{ fontFamily: 'Medium', fontSize: 15, color: '#ADACAC', paddingLeft: 7, fontWeight: 'bold', marginRight: 15 }}>Research & Development - ONL</Text>
-                </View>
-                <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                    <Text style={{ fontFamily: 'Medium', fontSize: 15, color: '#ADACAC', paddingLeft: 7, fontWeight: 'bold', marginRight: 15 }}>00082</Text>
-                </View>
-                <View style={{ marginTop: 10, marginHorizontal: 10 }}>
-                    <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Regarding</Text>
-                    <TextInput placeholder='Resolusi Tenaga Kerja/Penambahan SDM' style={{
-                        borderWidth: 1, fontSize: 10,
-                    }} />
-                </View>
-                <View style={{ marginTop: 10, marginHorizontal: 10 }}>
-                    <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Priority</Text>
-                    <View style={{borderWidth:1}}>
-                    <Picker
-                    selectedValue={selectPriority}
-                    onValueChange={(itemValue, itemIndex) => setSelectPriority(itemValue)}
-                    >
-                        <Picker.Item label="High" value="High"/>
-                        <Picker.Item label="Normal" value="Normal"/>
-                        <Picker.Item label="Urgent" value="Urgent"/>
-                        <Picker.Item label="Low" value="Low"/>
-                    </Picker>
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+
+                <Text style={{ fontFamily: 'Bold', fontSize: 30, textAlign: 'center', color: '#205375' }}>Buat Isu Baru</Text>
+                <ScrollView style={{ backgroundColor: '#FFFFFE', marginTop: 5, flex: 1 }}>
+
+                    <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+                        <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Judul Isu</Text>
+                        <TextInput placeholder='Masukan Judul' style={StyleGw.inputText} />
                     </View>
-                </View>
-                <View style={{ marginTop: 10, marginHorizontal: 10 }}>
-                    <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Status</Text>
-                    <TextInput placeholder='Open' style={{
-                        borderWidth: 1, fontSize: 10,
-                    }} />
-                </View>
-                <View style={{ marginTop: 10, marginHorizontal: 10 }}>
-                    <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Issue Type</Text>
-                    <TextInput placeholder='Recruitment' style={{
-                        borderWidth: 1, fontSize: 10,
-                    }} />
-                </View>
-                <View style={{ marginTop: 10, marginHorizontal: 10 }}>
-                    <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Explanation</Text>
-                    <TextInput placeholder='Lorem ipsum dolor sit amet, 
-                        consectetur adipiscing elit. Etiam eu turpis molestie, 
-                        dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-                        ' style={{
-                            borderWidth: 1, fontSize: 10,
-                        }} />
-                </View>
-                <View style={{ marginTop: 40, marginHorizontal: 20 }}>
-                    <Button status={"primary"} style={{
-                        borderWidth: 1, marginHorizontal: 100,
-                        borderRadius: 15, fontFamily: 'Regular'
-                    }}>Tambah</Button>
-                </View>
-            </ScrollView>
+                    <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+                        <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Prioritas</Text>
+                        <View style={{ borderWidth: 1, borderRadius: 5, borderColor: '#7F8487' }}>
+                            <Picker
+                                selectedValue={selectPriority}
+                                style={{ height: 25, fontFamily: 'Regular' }}
+                                onValueChange={(itemValue, itemIndex) => setSelectPriority(itemValue)}
+                            >
+                                <Picker.Item label="Pilih Prioritas" />
+                                {
+                                    priority.map((item) => {
+                                        return (
+                                            <Picker.Item label={item.name} value={item.name} key={item.name} />
+                                        )
+                                    })
+                                }
+                            </Picker>
+                        </View>
+                    </View>
+                    <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+                        <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Issue Type</Text>
+                        <TextInput placeholder='Recruitment' style={StyleGw.inputText} />
+                    </View>
+                    <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+                        <Text style={{ marginBottom: 5, fontFamily: 'Regular' }}>Explanation</Text>
+                        <TextInput placeholder='asd' style={StyleGw.inputText} />
+                    </View>
+                    <View style={{ marginTop: 40, marginHorizontal: 20 }}>
+                    </View>
+                </ScrollView>
+            </View>
+            <View>
+                <Button status={"primary"} style={{
+                    fontFamily: 'Regular'
+                }}>Tambah</Button>
+            </View>
         </View>
     )
 }
+
+const StyleGw = StyleSheet.create({
+    inputText: {
+        borderWidth: 1,
+        fontSize: 13,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        borderColor: '#7F8487'
+    }
+})
